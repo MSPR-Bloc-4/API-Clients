d3c72bd06c3bba27118e9116f450c5b53c76ff60
RKS-BlueSky <60184677+RKS-BlueSky@users.noreply.github.com>
2024-06-25 23:07:25 +0200

    Repo initialization and connection to firestore

---
+++ Client-Api/Configuration/FirebaseConfig.cs
@@ -0,0 +1,9 @@
+﻿namespace Client_Api.Configuration;
+
+public class FirebaseConfig
+{
+    public string ServiceAccountPath { get; set; }
+    public string ProjectId { get; set; }
+    public string ApiKey { get; set; }
+}
+
